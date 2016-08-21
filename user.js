1e37a0d4ef45dc64473562b1d4a4a82296f71bcb
femipixels <femi.systems@gmail.com>
2016-08-21 19:14:33 +0100

    Unit tests and Code refactor

--- user.js
+++ user.js
@@ -1,6 +1,3 @@
-// create user
-// search user (username)
-// validate user (username, password)
 var User = function() {
     this.users = [];
     this.currentUserId = 1;
@@ -11,14 +8,58 @@ var User = function() {
      *
      */
     this.createUser = function(username, password) {
-        
         var newUser = new UserDetails(username, password);
+        
+        if (typeof(newUser.username) === 'undefined') {
+            return 'A username and password is required';
+        }
+        
         newUser.userid = this.currentUserId;
         this.currentUserId++;
         this.users.push(newUser);
+        
+        return 'New user successfully created!';
     };
 
+    this.searchUser = function(username) {
+        var userExists = this.userExists(username);
+        
+        if (userExists === 'Invalid username') {
+            return 'Invalid username';
+        } else if (userExists === false) {
+            return 'User does not exist';
+        } else return this.users[userExists];
+    };
 
+    this.authUser = function(username, password) {
+        var userExists = this.userExists(username)
+        var users = this.users;
+
+        if (typeof(username) !== 'string' || typeof(password) !== 'string') {
+            return 'Please enter a username and password';
+        } else if (userExists === false) {
+            return 'Invalid login details';
+        } else {
+            if (users[userExists].username === username
+                && users[userExists].password === password) {
+                return 'Welcome, ' + username + '!';
+            }    
+        }
+
+        return 'Invalid login details';
+    };
+
+    this.userExists = function(username, password) {
+        if (typeof(username) !== 'string') return 'Invalid username';
+
+        for (var i = 0; i < this.users.length; i++) {
+            if (this.users[i].username === username) {
+                return i;
+            }
+        }
+
+        return false;
+    };
 
 };
 
@@ -30,15 +71,13 @@ var User = function() {
  *
  */
 var UserDetails = function(username, password) {
-    
-    if (username === undefined || password === undefined) {
-        return 'User Details incomplete';
+    if (typeof(username) === 'undefined' || typeof(password) === 'undefined') {
+        return false;
     } else {
-        this.username = username;
-        this.password = password;
+        this.username = username.toString();
+        this.password = password.toString();
         this.userid = 0;    
     }
-
 };
 
 module.exports = { 'User' : User, 'UserDetails' : UserDetails };
